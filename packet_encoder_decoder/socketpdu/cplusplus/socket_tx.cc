9967059e86e20fd8288829cd16a17f6e6cfce19b
willydlw <willydlw@comcast.net>
2018-10-10 16:03:53 -0600

    corrected comment block in send_to_socket_pdu. Adding socket_tx.cc  This program starts a TCP server, sends the data received to a tagged stream block, and writes the stream data in a file

--- packet_encoder_decoder/socketpdu/cplusplus/socket_tx.cc
+++ packet_encoder_decoder/socketpdu/cplusplus/socket_tx.cc
@@ -22,8 +22,13 @@
 
 /*
  * GNU Radio C++ example creating a socket tcp server
- * that receives data from a client and prints the data
- * to standard output.
+ * that receives data from a client and prints the 
+ * received data to standard output.
+ * 
+ * The protocol data unit must be converted to a stream
+ * for processing, via the PDU to Tagged Stream function.
+ * 
+ * The stream data is written to a file.
  *
  * Input
  *  Socket Protocol Data Unit
@@ -41,6 +46,9 @@
 #include <gnuradio/top_block.h>
 #include <gnuradio/blocks/message_debug.h>
 #include <gnuradio/blocks/socket_pdu.h>
+#include <gnuradio/blocks/pdu_to_tagged_stream.h>
+#include <gnuradio/blocks/file_sink.h>
+#include <gnuradio/blocks/tag_debug.h>
 
 using namespace gr;
 
@@ -57,8 +65,29 @@ int main(int argc, char **argv)
   // Construct a message debug block
   blocks::message_debug::sptr message_debug_0 = blocks::message_debug::make();
 
+  // Construct a tag debug block
+  blocks::tag_debug::sptr tag_debug_0 = blocks::tag_debug::make(sizeof(char), "", "");
+  tag_debug_0->set_display(true);
+
+  // Construct a pdu_to_tagged_stream block, stream data type is byte,
+  // name of the tag that specified packet length
+  blocks::pdu_to_tagged_stream::sptr pdu_to_tagged_stream_0 = 
+                blocks::pdu_to_tagged_stream::make(blocks::pdu::byte_t,"packet_len");
+
+  // Construct a file sink to store data stream
+  blocks::file_sink::sptr file_sink_0 = blocks::file_sink::make(sizeof(char), "tag_stream.bin");
+  /*##################################################
+  # Connections
+  ################################################## */
+  
   // Connect socket pdu output to message debug print pdu input
   tb->msg_connect(socket_pdu_0, "pdus", message_debug_0, "print_pdu");
+
+  // Connect socket pdu output to pdu to tagged stream input
+  tb->msg_connect(socket_pdu_0, "pdus", pdu_to_tagged_stream_0, "pdus");
+
+  tb->connect(pdu_to_tagged_stream_0, 0, tag_debug_0, 0);
+  tb->connect(pdu_to_tagged_stream_0, 0, file_sink_0, 0);
   
 
   // Tell GNU Radio runtime to start flowgraph threads; the foreground thread
@@ -68,7 +97,28 @@ int main(int argc, char **argv)
   // Real applications may use tb->start() which returns, allowing the foreground
   // thread to proceed, then later use tb->stop(), followed by tb->wait(), to cleanup
   // GNU Radio before exiting.
-  tb->run();
+  //tb->run();
+
+  tb->start();
+  
+  // equivalent of python's raw_input function
+  // program terminates normally when user presses enter key
+  // If program does not terminate normally (Ctrl + C), data will not be
+  // written to the output file.
+  try {
+    std::string quitString;
+    std::cout << "Press enter to quit: ";
+    getline(std::cin, quitString);
+  }
+  catch(int e){
+    std::cout << "An exception occurred. Exception number: " << e << '\n';
+  }
+
+  tb->stop();
+  tb->wait();
+
+  
+
 
   // Exit normally.
   return 0;
